8e06d453901b98d41cb07a57abb09773f7697d98
Genrry Luis <gluis-2020232@kinal.edu.gt>
2022-03-09 03:16:07 -0600

    Conexion a la base de datos

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors'); //Cabecera
+const app = express();
+
+//IMPORTACION RUTAS
+//const empleadosRoutes = require('./src/routes/empleados.routes');
+//const empresasRoutes = require('./src/routes/empresas.routes');
+//const usuarioRoutes = require('./src/routes/usuario.routes');
+
+
+//MIDDLEWARE : Un middleware es un intermedio y un verificador de etc
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json()); //Convertir a json todo
+
+//CABECERAS
+app.use(cors()); //Cargo la cabecera
+
+
+// CARGA DE RUTAS localhost:3000
+//app.use('/api', empleadosRoutes, empresasRoutes, usuarioRoutes);
+
+module.exports = app;
