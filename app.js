6c6be5423b525d1b3959d70900fc9b8c23105cec
Genrry Luis <gluis-2020232@kinal.edu.gt>
2022-03-10 00:25:44 -0600

    Método Registrar

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 //IMPORTACION RUTAS
 //const empleadosRoutes = require('./src/routes/empleados.routes');
 //const empresasRoutes = require('./src/routes/empresas.routes');
-//const usuarioRoutes = require('./src/routes/usuario.routes');
+const usuarioRutas = require('./src/routes/usuario.routes');
 
 
 //MIDDLEWARE : Un middleware es un intermedio y un verificador de etc
@@ -17,6 +17,6 @@ app.use(cors()); //Cargo la cabecera
 
 
 // CARGA DE RUTAS localhost:3000
-//app.use('/api', empleadosRoutes, empresasRoutes, usuarioRoutes);
+app.use('/api', usuarioRutas);
 
 module.exports = app;
