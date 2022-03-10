85bf76096b52f268f53a478fb8457d22c97239a0
Genrry Luis <gluis-2020232@kinal.edu.gt>
2022-03-10 00:59:38 -0600

    Creación del Login

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,8 +5,8 @@ const md_autenticacion = require('../middlewares/autenticacion');
 const api = express.Router();
 
 api.post('/registrar', usuarioControlador.Registrar);
-//api.post('/registrarMaestro', usuarioControlador.RegistrarMaestro);
-//api.post('/login', usuarioControlador.Login);
-//api.put('/editarUsuario/:idUsuario', md_autenticacion.Auth, usuarioControlador.EditarUsuario);
+api.post('/registrarMaestro', usuarioControlador.RegistrarAdmin);
+api.post('/login', usuarioControlador.Login);
+api.put('/editarUsuario/:idUsuario', md_autenticacion.Auth, usuarioControlador.EditarUsuario);
 
 module.exports = api;
