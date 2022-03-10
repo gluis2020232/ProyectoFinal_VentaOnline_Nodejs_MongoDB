0d7cdbd225590f2759a5635091d2028409903e83
Genrry Luis <gluis-2020232@kinal.edu.gt>
2022-03-10 00:56:13 -0600

    Método Login

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -37,7 +37,96 @@ function Registrar(req, res) {
     }
 }
 
+function RegistrarAdmin(req, res) {
+    var parametros = req.body;
+    var usuarioModel = new Usuario();
+
+
+    usuarioModel.nombre = 'MAESTRO';
+    usuarioModel.apellido = 'MAESTRO';
+    usuarioModel.email = 'MAESTRO';
+    usuarioModel.rol = 'ROL_MAESTRO';
+    usuarioModel.imagen = null;
+
+    Usuario.find({ email : 'MAESTRO' }, (err, usuarioEncontrado) => {
+        if ( usuarioEncontrado.length == 0 ) {
+
+            bcrypt.hash('123456', null, null, (err, passwordEncriptada) => {
+                usuarioModel.password = passwordEncriptada;
+
+                usuarioModel.save((err, usuarioGuardado) => {
+                    if (err) return res.status(500)
+                        .send({ mensaje: 'Error en la peticion' });
+                    if(!usuarioGuardado) return res.status(500)
+                        .send({ mensaje: 'Error al agregar el Usuario'});
+                    
+                    return res.status(200).send({ usuario: usuarioGuardado });
+                });
+            });                    
+        } else {
+            return res.status(500)
+                .send({ mensaje: 'Este correo, ya  se encuentra utilizado' });
+        }
+    })
+    
+}
+
+
+function Login(req, res) {
+    var parametros = req.body;
+    Usuario.findOne({ email : parametros.email }, (err, usuarioEncontrado)=>{
+        if(err) return res.status(500).send({ mensaje: 'Error en la peticion' });
+        if(usuarioEncontrado){
+            // COMPARO CONTRASENA SIN ENCRIPTAR CON LA ENCRIPTADA
+            bcrypt.compare(parametros.password, usuarioEncontrado.password, 
+                (err, verificacionPassword)=>{//TRUE OR FALSE
+                    // VERIFICO SI EL PASSWORD COINCIDE EN BASE DE DATOS
+                    if ( verificacionPassword ) {
+                        // SI EL PARAMETRO OBTENERTOKEN ES TRUE, CREA EL TOKEN
+                        if(parametros.obtenerToken === 'true'){
+                            return res.status(200)
+                                .send({ token: jwt.crearToken(usuarioEncontrado) })
+                        } else {
+                            usuarioEncontrado.password = undefined;
+                            return  res.status(200)
+                                .send({ usuario: usuarioEncontrado })
+                        }
+
+                        
+                    } else {
+                        return res.status(500)
+                            .send({ mensaje: 'Las contrasena no coincide'});
+                    }
+                })
+
+        } else {
+            return res.status(500)
+                .send({ mensaje: 'Error, el correo no se encuentra registrado.'})
+        }
+    })
+}
+
+function EditarUsuario(req, res) {
+    var idUser = req.params.idUsuario;
+    var parametros = req.body;    
+
+    if ( idUser !== req.user.sub ) return res.status(500)
+        .send({ mensaje: 'No puede editar otros usuarios'});
+
+    Usuario.findByIdAndUpdate(req.user.sub, parametros, {new : true},
+        (err, usuarioActualizado)=>{
+            if(err) return res.status(500)
+                .send({ mensaje: 'Error en la peticion' });
+            if(!usuarioActualizado) return res.status(500)
+                .send({ mensaje: 'Error al editar el Usuario'});
+            
+            return res.status(200).send({usuario : usuarioActualizado})
+        })
+}
 
 module.exports = {
-    Registrar
+    Registrar,
+    RegistrarAdmin, 
+    Login,
+    EditarUsuario
 }
