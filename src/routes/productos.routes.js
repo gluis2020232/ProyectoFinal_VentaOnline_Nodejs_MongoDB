274e8b1c6c92cc4b308d1324c60c66e2f8acee5f
Genrry Luis <gluis-2020232@kinal.edu.gt>
2022-03-11 01:38:43 -0600

    Metodo Visualizar productos

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -10,7 +10,9 @@ const md_roles = require('../middlewares/roles')
 //RUTAS
 const api = express.Router();
 
-//api.get('/productos', productosControlador.ObtenerProductos);
+api.get('/obtenerProductos', [md_autenticacion.Auth, md_roles.verAdminVisua], controladorProducto.obtenerProductos);
 api.post('/agregarProductos', [md_autenticacion.Auth, md_roles.verAdmin], controladorProducto.agregarProductos);
+api.put('/editarProducto/:idProducto', [md_autenticacion.Auth, md_roles.verAdminEdit], controladorProducto.editarProducto);
+api.delete('/eliminarProducto/:idProducto', [md_autenticacion.Auth, md_roles.verAdminDelete], controladorProducto.eliminarProducto);
 
 module.exports = api;
